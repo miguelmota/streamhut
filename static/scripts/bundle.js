fe4a252dfa6a35c5314d3e91e26c39337cede22a
Miguel Mota <miguelmota2@gmail.com>
2017-03-20 00:23:17 -0700

    add players + responsive

--- static/scripts/bundle.js
+++ static/scripts/bundle.js
@@ -9166,41 +9166,89 @@ file.addEventListener(`change`, event => {
 
 stream.pipe(through(data => {
   console.log(`incoming:`, data.substr(0,20).concat(`...`));
+  const doc = document.createDocumentFragment();
   const el = create(`div`);
+  el.classList.add(`item`);
+
+  let mime = null;
+  let blob = null;
+  let dd = data;
+
+  if (/data:/gi.test(data)) {
+    const d1 = data.substr(0,32).replace(/.*base64,/gi, ``);
+    const d2 = data.substr(32, data.length - 1);
+    dd = d1.concat(d2);
+    mime = base64Mime(data) || ``;
+    blob = base64ToBlob(dd, mime);
+  } else {
+    mime = `text/plain`;
+    blob = new Blob([data], {type: mime});
+  }
+
+  let ext = mime.split(`/`).join(`_`).replace(/[^\w\d_]/gi, ``);
+  const url = window.URL.createObjectURL(blob);
+
+  const tpd = create(`div`);
+  tpd.appendChild(create(`text`)(`${blob.type} size:${blob.size}B`));
+  doc.appendChild(tpd);
+
+  const a = create(`a`);
+  a.appendChild(create(`text`)(url));
+  a.href = url;
+  a.target = `_blank`;
+  doc.appendChild(a);
+
+  const dv = create(`article`);
+
+  if (/image/gi.test(mime)) {
+    const img = create(`img`);
+    img.src = data;
+    dv.appendChild(img);
+  } else if (/video/gi.test(mime)) {
+    const dv = create(`div`);
+    const vid = create(`video`);
+    vid.src = url;
+    vid.controls = `controls`;
+    dv.appendChild(vid);
+  } else if (/audio/gi.test(mime)) {
+    const aud = create(`audio`);
+    aud.src = url;
+    aud.controls = `controls`;
+    dv.appendChild(aud);
+  } else if (/(json|javascript|text)/gi.test(mime)) {
+    const t = atob(data.replace(/.*base64,/gi, ``));
+    const pr = create(`pre`);
+    const tt = create(`text`)(t);
+    pr.appendChild(tt);
+    dv.appendChild(pr);
+  } else {
+    const pr = create(`pre`);
+    const t = create(`text`)(data);
+    pr.appendChild(t);
+    dv.appendChild(pr);
+  }
+
+  doc.appendChild(dv);
+
+  const filename = `${Date.now()}_${ext}`;
+  const dla = create(`a`);
+  dla.className = `download`;
+  dla.href = url;
+  dla.download = filename;
+  const dlat = create(`text`)(`download`);
+  dla.appendChild(dlat);
+
+  const btd = create(`footer`);
+
+  btd.appendChild(dla)
+
   const dt = create(`time`);
   const dtt = create(`text`)((new Date()).toString());
   dt.appendChild(dtt);
-  el.classList.add(`item`);
-  if (/data:/gi.test(data)) {
-    const a = create(`a`);
-    const d1 = data.substr(0,32).replace(/.*base64,/gi, ``);
-    const d2 = data.substr(32, data.length - 1);
-    const dd = d1.concat(d2);
-    const blob = base64ToBlob(dd, base64Mime(data));
-    const url = window.URL.createObjectURL(blob);
-    a.appendChild(create(`text`)(url));
-    a.href = url;
-    a.target = `_blank`;
-    const d = create(`div`);
-    d.appendChild(create(`text`)(`${blob.type} size:${blob.size}`));
-    el.appendChild(d);
-    el.appendChild(a);
+  btd.appendChild(dt)
+  doc.appendChild(btd);
 
-    if (/data:image\/(png|jpe?g)/gi.test(data)) {
-      const dv = create(`div`);
-      const img = create(`img`);
-      img.src = data;
-      dv.appendChild(img);
-      el.appendChild(dv);
-    }
-  } else {
-    const d = create(`pre`);
-    const t = create(`text`)(data);
-    d.appendChild(t);
-    el.appendChild(d);
-  }
-
-  el.appendChild(dt);
+  el.appendChild(doc);
   output.insertBefore(el, output.firstChild);
 }));
 
